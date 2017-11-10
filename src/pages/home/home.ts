f5ca2986e0ac9fc17306a2abd62d9b8072c893b8
gmDefault <cedric.lafrasse@gmail.com>
2017-11-10 16:17:03 +0100

    q9-v1

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+interface Search {
+  title : string;
+  author : string;
+  date : string;
+  image : string;
+}
+
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  result_table: Search[] = [{ title: "FightClub", author: "Moi", date: "30-FEB-2020", image: "http://lorempixel.com/400/200" }];
   constructor(public navCtrl: NavController) {
 
   }
