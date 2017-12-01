97c1a69c97fa295e58c1fed84033f8f64858ea5e
gmDefault <cedric.lafrasse@gmail.com>
2017-12-01 15:52:38 +0100

    Modif Search

--- src/app/tmdb.ts
+++ src/app/tmdb.ts
@@ -1,1 +1,2 @@
 
+export const api_key = "ebb02613ce5a2ae58fde00f4db95a9c1";
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { DetailsPage } from '../details/details';
+import {api_key} from "../../app/tmdb"
+
 
 export interface Search {
   title : string;
   author : string;
-  date : string;
-  image : string;
+  release_date : string;
+  poster_path : string;
+  overview : string;
 }
 
 const resultat_table : Array<Search> = [];
@@ -17,7 +20,7 @@ const resultat_table : Array<Search> = [];
   templateUrl: 'home.html'
 })
 export class HomePage {
-  result_table: Search[] = [{ title: "FightClub", author: "Moi", date: "30-FEB-2020", image: "http://www.lorempixel.com/400/200" }];
+  result_table: Search[] = [{ title: "FightClub", author: "Moi", release_date: "30-FEB-2020", poster_path: "http://www.lorempixel.com/400/200", overview: "Blablablablablabla"}];
   constructor(public navCtrl: NavController) {
   }
 query : string = "";
@@ -29,5 +32,19 @@ push(item:Search):void{
 
 ionInput():void{
   console.log(this.query);
+  var data = "{}";
+
+  var xhr = new XMLHttpRequest();
+  xhr.withCredentials = true;
+
+  xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === this.DONE) {
+      console.log(this.responseText);
+    }
+  });
+
+  xhr.open("GET", "https://api.themoviedb.org/3/search/movie?include_adult=false&page=1&language=en-US&api_key=%3C%3Capi_key%3E%3E");
+
+  xhr.send(data);
 }
 }
