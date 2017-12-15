957bac716d4f932fab2e45ca04f8b762422401ed
gmDefault <cedric.lafrasse@gmail.com>
2017-12-15 14:07:35 +0100

    v1TP2

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,7 +2,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
 
-import 'rxjs/add/observable/of'
-import 'rxjs/add/operator/pluck'
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/pluck';
+import 'rxjs/add/operator/switchMap';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
